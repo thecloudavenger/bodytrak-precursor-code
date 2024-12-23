7a39095f3c49d73c730c59cde99172231c1c4389
todhanyasri <132885372+todhanyasri@users.noreply.github.com>
2024-12-23 15:00:23 +0000

    Adding Migrations

--- store/models.py
+++ store/models.py
@@ -6,8 +6,9 @@ class Promotion(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.IntegerField
     last_update = models.DateTimeField(auto_now=True)
     promotions = models.ManyToManyField(Promotion)
