b8b7cc9d94680d91057b47e0f29dc22ab83dfbd2
todhanyasri <132885372+todhanyasri@users.noreply.github.com>
2024-12-24 13:19:24 +0000

    Adding nested router for future cart & cartitem

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 
-urlpatterns = router.urls
+product_router = routers.NestedDefaultRouter(router,'products',lookup='product')
+
+
+urlpatterns = router.urls + product_router.urls
