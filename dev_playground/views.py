b513c8b1cc50d5496233922744a16659b4d97ae0
todhanyasri <132885372+todhanyasri@users.noreply.github.com>
2024-12-30 06:32:02 +0000

    Minor formatiing

--- dev_playground/views.py
+++ dev_playground/views.py
@@ -3,10 +3,4 @@ from django.http import HttpResponse
 from store.models import Product
 
 def say_hello(request):
-    #try:
-    #     send_mail('subject','message','thecloudavenger@gmail.com',['thecloudavenger@gmail.com'])
-
-    #     mail_admins('subject','message',html_message='message')
-    # except BadHeaderError:
-    #     pass
-    return render(request,'hello.html', {'name': 'Dhanya'})
+    return render(request,'hello.html', {'name': 'Tes'})
