5525c56529a11a95630cedc7299cb6bb5b23760b
todhanyasri <132885372+todhanyasri@users.noreply.github.com>
2024-12-31 15:41:29 +0000

    Adding save cart option

--- store/views.py
+++ store/views.py
@@ -4,11 +4,11 @@ from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveM
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, ValidationError
 from rest_framework import status
 from store.permissions import IsAdminOrReadOnly
 from .models import Cart, CartItem, Customer, Order, Product
-from .serializers import AddCartItemSerializer, CartItemSerializer, CartSerializer, CreateOrderSerializer, CustomerSerializer, OrderSerializer, ProductSerializer, UpdateCartItemSerializer, UpdateOrderSerializer
+from .serializers import AddCartItemSerializer, CartItemSerializer, CartSerializer, CreateCartSerializer, CreateOrderSerializer, CustomerSerializer, OrderSerializer, ProductSerializer, UpdateCartItemSerializer, UpdateOrderSerializer
 import logging
 logger = logging.getLogger(__name__) 
 
@@ -27,13 +27,46 @@ class ProductViewSet(ModelViewSet):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class CartViewSet(CreateModelMixin,
-                  RetrieveModelMixin,
-                  DestroyModelMixin,
-                  GenericViewSet):
+class CartViewSet(ModelViewSet):
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
 
+    def create(self,request, *args, **kwargs): #Overriding the create
+        serializer = CreateCartSerializer(data = request.data,
+                                           context={'user_id': self.request.user.id})
+        serializer.is_valid(raise_exception = True)
+
+        try:
+            customer = Customer.objects.only('id').get(user_id=self.request.user.id)
+        except Customer.DoesNotExist:
+            raise NotFound(detail="Customer not found for the current user.")
+
+        if Cart.objects.filter(customer=customer).exists():
+            raise ValidationError(detail="Customer already has a cart.")
+        
+        cart  = serializer.save()
+        serializer = CreateCartSerializer(cart)
+        return Response(serializer.data)
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateCartSerializer
+        return CartSerializer
+
+    def get_serializer_context(self):
+        return {'user_id': self.request.user.id} #has to be passed then only its available
+
+    def get_queryset(self):
+        user = self.request.user
+        try:
+            customer_id = Customer.objects.only('id').get(user_id=user.id).id
+        except Customer.DoesNotExist:
+            raise NotFound(detail="Customer not found for the current user.")
+
+        return Cart.objects.filter(customer_id = customer_id)
+    
+    
 class CartItemViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
    
